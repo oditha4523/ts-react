8d85fe5bf8cb3393457d8f3f089194cc8f215944
oditha4523 <odithachamod@gmail.com>
2026-01-03 21:42:07 +0530

    fix: update shadow style in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo - Left Corner */}
